5b5080320c853c9fa30689efcaefeb9798023bf0
Michel Hognerud <leyou.m@gmail.com>
2014-06-29 22:25:36 +0800

    Exporting templating package

---
+++ TemplateInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace Asgard\Templating;
+
+interface TemplateInterface {
+	public function setEngine(TemplateEngineInterface $engine);
+
+	public function setTemplate($template);
+
+	public function getTemplate();
+
+	public function setParams(array $params=[]);
+
+	public function getParams();
+
+	public function render($template=null, array $params=[]);
+
+	public static function renderFile($file, array $params=[]);
+}
